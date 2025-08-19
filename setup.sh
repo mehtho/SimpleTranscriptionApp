d2a02763745b046f8d88395202f20bf7d6fa510e
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2025-08-19 15:20:40 +0000

    Basic ping endpoint

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Installs UV. Restart terminal afterwards to apply changes.
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-# Install deps from requirements.txt to UV
-uv add -r requirements.txt
+# Install deps from requirements.txt to UV in the asr directory
+cd asr && uv add -r requirements.txt && uv sync
