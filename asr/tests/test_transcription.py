6f6d72856dc56bf281e35b7e9ae3096412169b7f
Ubuntu <azureuser@matt-dev-vm.hnzzbeq2torepl51ogqmpiw0zf.bx.internal.cloudapp.net>
2025-08-19 16:26:13 +0000

    Simple transcription test

---
+++ asr/tests/test_transcription.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_asr_simple():
+    with open("tests/short_audio.mp3", "rb") as f:
+        response = client.post("/asr", files={"file": ("short_audio.mp3", f, "audio/mpeg")})
+    assert response.status_code == 200
+    data = response.json()
+    assert "transcription" in data
+    assert "duration" in data
