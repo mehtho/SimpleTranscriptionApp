3d8e921225d83c6a2d775b004e63071c325a284c
Ubuntu <azureuser@matt-dev-vm.hnzzbeq2torepl51ogqmpiw0zf.bx.internal.cloudapp.net>
2025-08-19 16:10:52 +0000

    Added simple test setup

--- setup.sh
+++ setup.sh
@@ -8,8 +8,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Install deps from requirements.txt to UV in the asr directory
 (cd asr && \
-    uv add -r requirements.txt && \
-    uv sync)
+    uv sync --extra testing)
 
 # Install ruff
 (cd linters && \
