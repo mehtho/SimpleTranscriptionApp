2fcfe1d63dbeaf28482782fbb06640fd8a6cf697
Ubuntu <azureuser@matt-dev-vm.hnzzbeq2torepl51ogqmpiw0zf.bx.internal.cloudapp.net>
2025-08-19 15:29:51 +0000

    Skeleton file upload endpoint

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Install deps from requirements.txt to UV in the asr directory
 cd asr && uv add -r requirements.txt && uv sync
+
+mkdir -p models && cd models && git clone https://huggingface.co/facebook/wav2vec2-large-960h
