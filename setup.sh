123cec2f82fc2e7f9fe7a77f41864304ef360157
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2025-08-19 16:26:13 +0000

    Simple transcription test

--- setup.sh
+++ setup.sh
@@ -6,10 +6,19 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
     cd models && \
     git clone https://huggingface.co/facebook/wav2vec2-large-960h)
 
-# Install deps from requirements.txt to UV in the asr directory
+# Add the "unzip" program
+(sudo apt-get update && \
+    sudo apt-get install unzip)
+
+# Download the test dataset
+curl -L -o common_voice.zip "https://www.dropbox.com/scl/fi/i9yvfqpf7p8uye5o8k1sj/common_voice.zip?rlkey=lz3dtjuhekc3xw4jnoeoqy5yu&e=1&dl=1"
+unzip common_voice.zip -d test_data
+# rm common_voice.zip
+
+# Install deps from requirements.txt to UV in the asr microservice directory
 (cd asr && \
     uv sync --extra testing)
 
-# Install ruff
-(cd linters && \
+# Install linters dependencies
+(cd linter && \
     uv sync)
